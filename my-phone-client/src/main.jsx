b8953b5c9f9080ab610e027278a95b355928fcd3
jisanahamed09205 <jisanahamed09205@gmail.com>
2023-10-23 07:42:00 +0600

    first commit

---
+++ my-phone-client/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/Main';
+import Phone from './Components/Phone';
+import Phones from './Components/Phones';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/phones",
+        element: <Phones />,
+        loader: () => fetch('http://localhost:4000/phones')
+      },
+      {
+        path: '/phone/:id',
+        element: <Phone></Phone>,
+        loader: ({params}) => fetch(`http://localhost:4000/phones/${params.id}`)
+      }
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
